a979919e97f44df5f1f5161e52b05c2caa3e2e9c
M. Edward (Ed) Borasky <znmeb@znmeb.net>
2018-02-02 21:35:42 -0800

    no database creation in dump file issue #1

--- ingest.sh
+++ ingest.sh
@@ -3,5 +3,5 @@
 echo "Creating the database"
 psql < /home/postgres/ingest.psql
 echo "Creating the database dump"
-pg_dump --verbose --clean --if-exists --create --dbname=passenger_census \
+pg_dump --verbose --clean --if-exists --dbname=passenger_census \
   > /home/postgres/passenger_census.sql
