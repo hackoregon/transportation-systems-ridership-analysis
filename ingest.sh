da9fe80ce2feda2b3d2c2ab7c99cf91506b70c74
M. Edward (Ed) Borasky <znmeb@znmeb.net>
2018-02-07 21:03:23 -0800

    compress the dump!

--- ingest.sh
+++ ingest.sh
@@ -3,5 +3,5 @@
 echo "Creating the database"
 psql < /home/postgres/ingest.psql
 echo "Creating the database dump"
-pg_dump --verbose --clean --if-exists --dbname=passenger_census \
-  > /home/postgres/passenger_census.sql
+pg_dump --format=custom --verbose --clean --if-exists --dbname=passenger_census \
+  > /home/postgres/passenger_census.pgdump
