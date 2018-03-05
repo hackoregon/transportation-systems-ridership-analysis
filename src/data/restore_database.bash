c66bbb1abc36659587d5dedd98272ab84659974a
M. Edward (Ed) Borasky <znmeb@znmeb.net>
2018-03-05 01:42:45 -0800

    add some comments

--- src/data/restore_database.bash
+++ src/data/restore_database.bash
@@ -2,6 +2,9 @@
 
 set +e # continue on error
 
+echo "Dropping 'passenger_census'"
 dropdb passenger_census
+echo "Creating 'passenger_census'"
 createdb passenger_census
+echo "Restoring 'passenger_census'"
 pg_restore --no-owner --dbname=passenger_census ../../data/interim/passenger_census.backup
