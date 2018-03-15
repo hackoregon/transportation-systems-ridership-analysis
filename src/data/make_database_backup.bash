92202dd40374174c6336b91771c6baec6d5259e4
M. Edward (Ed) Borasky <znmeb@znmeb.net>
2018-03-15 10:31:21 -0700

    ready for smoke test

--- src/data/make_database_backup.bash
+++ src/data/make_database_backup.bash
@@ -3,10 +3,10 @@
 # assumes
 # 1. Linux,
 # 2. PostgreSQL with trust authentication for your user ID, You do *not* need PostGIS!
-# 3. Your user ID has database creation privileges.
+# 3. Your user ID has superuser privileges.
 
 echo "Creating the database"
 psql -f make_database.sql
 echo "Creating the database backup"
-pg_dump --format=custom --verbose --dbname=passenger_census \
-  > ../../data/interim/passenger_census.backup
+pg_dump --format=p --verbose --clean --create --if-exists --dbname=passenger_census \
+  | gzip -c > ../../data/interim/passenger_census.sql.gz
--- src/data/restore_database.bash
+++ src/data/restore_database.bash
@@ -1,10 +1,9 @@
 #! /bin/bash
 
-set +e # continue on error
+# assumes
+# 1. Linux,
+# 2. PostgreSQL with trust authentication for your user ID, You do *not* need PostGIS!
+# 3. Your user ID has superuser privileges.
 
-echo "Dropping 'passenger_census'"
-dropdb passenger_census
-echo "Creating 'passenger_census'"
-createdb passenger_census
 echo "Restoring 'passenger_census'"
-pg_restore --no-owner --dbname=passenger_census ../../data/interim/passenger_census.backup
+gzip -dc ../../data/interim/passenger_census.sql.gz | psql
